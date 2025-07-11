5455db536ed2cf388c3732d891ac3f1d2bec717f
Luqman Hassan <luqman1isb@gmail.com>
2025-07-11 17:30:44 +0500

    added simple CRUD operation for task managemant APIs feature not add yet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes")
 const userRoleRoutes = require("./routes/userRoleRoutes")
+const taskRoutes =  require("./routes/taskRoutes")
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/userRole" , userRoleRoutes)
+app.use("/api/taskRoutes", taskRoutes)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running");
